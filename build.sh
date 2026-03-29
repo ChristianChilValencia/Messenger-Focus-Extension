488be7a63e176abdac898499a78d893c49d16b18
ChristianChilValencia <valenciamyth@gmail.com>
2026-03-29 19:18:13 +0800

    Add Instagram icon to popup icons directory

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 set -e
 rm -rf dist
 npx tsc
-mkdir -p dist/popup
+mkdir -p dist/popup/icons
 cp src/popup/popup.html dist/popup/
 cp src/popup/popup.css dist/popup/
+cp src/popup/icons/* dist/popup/icons/
 echo "Build complete!"
